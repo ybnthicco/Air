4fe89d8a0c87f9133ae7d3c49dc2b6d622d30cb2
ybnthicco <75510362+ybnthicco@users.noreply.github.com>
2024-02-22 11:21:22 +0300

    Update func.php

--- func.php
+++ func.php
@@ -1,1 +1,13 @@
-<?php function permalink($var){$var=preg_replace('#([\W_]+)#',' ',$var);$var=str_replace(' ','-',$var);$var=strtolower($var);$var=rtrim($var,'-');;return $var;}function cleartitle($var){$var=str_replace('-',' ',$var);return $var;}?>
+<?php function permalink($var)
+{
+    $var = preg_replace("#([\W_]+)#", " ", $var);
+    $var = str_replace(" ", "-", $var);
+    $var = strtolower($var);
+    $var = rtrim($var, "-");
+    return $var;
+}
+function cleartitle($var)
+{
+    $var = str_replace("-", " ", $var);
+    return $var;
+} ?>
